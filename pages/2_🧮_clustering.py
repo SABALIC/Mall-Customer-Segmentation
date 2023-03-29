0c2c6672adef9827313c3e86b77958fe27ac83ed
Fatih Gurbuz <fatihogurbuz@gmail.com>
2023-03-29 23:31:56 +0200

    cluster half commit

--- pages/2_🧮_clustering.py
+++ pages/2_🧮_clustering.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
     page_title="Clustering",
@@ -6,6 +7,26 @@ st.set_page_config(
 )
 
 st.write("Based on the analysis conducted [here](https://github.com/furbuz/Mall-Customer-Segmentation/blob/main/Customer_Segmentation_and_Classification.ipynb) the data can be clustered into 6 Segments.")
+
+model_code = '''
+cluster_range = [i for i in range(2, 20)]
+inertias = []
+
+for c in cluster_range:
+  kmeans = KMeans(n_clusters = c, random_state = 42).fit(X)
+  inertias.append(kmeans.inertia_)
+'''
+
+st.code(model_code, language='python')
+
+kmeans_image = Image.open('k-means.png')
+st.image(kmeans_image)
+
+st.subheader("Elbow method yields 6 as number of clusters")
+
+
+
+
 
 col1, col2, col3 = st.columns(3)
 
